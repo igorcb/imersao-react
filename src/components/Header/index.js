a4c0f6ca1aedbab27cfa0c1587ce3011cde77dda
Igor C. Batista <igor.batista@gmail.com>
2021-01-25 20:28:04 -0300

    meta tag og image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ export default function Header(props) {
   return (
     <Head>
       <meta property="og:title" content=">Imersão React Alura" key="title" />
-      <meta property="og:image" content="https://i2.wp.com/css-tricks.com/wp-content/uploads/2017/06/css-is-awesome-scaled.jpg?resize=1536%2C1208&ssl=1" />
+      <meta property="og:image" content="https://image.freepik.com/psd-gratuitas/fundo-de-madeira-realista_1022-59.jpg" />
     </Head>
   );
 }
